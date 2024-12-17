012c7689bfc49023fa94313d7365355856cb61ba
Sahay <kuldeep.sahay@dxc.com>
2024-12-17 13:44:50 +0530

    9th  commit

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
+
+provider "google" {
+  project = "tf-cloud-build-445006"
+  region  = "us-central1"
+}
+
 resource "google_storage_bucket" "my_bucket" {
-  name          = "7903032868"
-  location      = "us-central1"
+  name     = "7903032868"
+  location = "US"
 }

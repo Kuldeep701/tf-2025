9fb40b3bfe0e07137c8d391efe93a11f755e62df
Tinku <Kuldeepsahay702@gmail.com>
2025-01-04 18:57:55 +0530

    Created bucket and vpc

--- main.tf
+++ main.tf
@@ -8,3 +8,7 @@ resource "google_storage_bucket" "my_bucket" {
   name     = "7250767165"
   location = "US"
 }
+resource "google_compute_network" "tf-ecom-vpc"{
+    name = "tf-auto-ecom-vpc"
+    auto_create_subnetworks = "false"
+}

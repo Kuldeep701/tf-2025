bf07ba18b976a78b7c477c33d60b48638f3682b9
Kuldeep701 <145199980+Kuldeep701@users.noreply.github.com>
2025-01-04 19:07:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,3 @@ resource "google_storage_bucket" "my_bucket" {
   name     = "7250767165"
   location = "US"
 }
-resource "google_compute_network" "tf-ecom-vpc"{
-    name = "tf-auto-ecom-vpc"
-    auto_create_subnetworks = false
-}

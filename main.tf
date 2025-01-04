110932edf60dc92cbe6f2a5346c44a6a6fb98835
Kuldeep701 <145199980+Kuldeep701@users.noreply.github.com>
2025-01-04 19:13:54 +0530

    added backend

--- main.tf
+++ main.tf
@@ -8,3 +8,9 @@ resource "google_storage_bucket" "my_bucket" {
   name     = "fdsblsdjnds"
   location = "US"
 }
+terraform {
+  backend "gcs" {
+    bucket = "fdsblsdjnds"
+    prefix = "terraform/state"
+  }
+}

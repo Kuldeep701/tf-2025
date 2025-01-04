1f1a5c97c233d578fe7caf98f26382d1c371539a
Kuldeep701 <145199980+Kuldeep701@users.noreply.github.com>
2025-01-04 17:38:38 +0530

    Updated main.tf for kuldeep702 new project

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 
 provider "google" {
-  project = "tf-cloud-build-445006"
+  project = "kuldeep702"
   region  = "us-central1"
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name     = "7903032868"
+  name     = "7250767165"
   location = "US"
 }

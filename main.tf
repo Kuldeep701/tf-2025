f418f0d2a2d11df612fafc37bf06d3e1d5af689b
Kuldeep701 <145199980+Kuldeep701@users.noreply.github.com>
2025-01-05 16:11:45 +0530

    2nd commit

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ terraform {
   backend "gcs" {
     bucket = "statefile3957"
     prefix = "terraform_state"
-    region  = "us-central1"
   }
 }
 
@@ -15,5 +14,3 @@ resource "google_storage_bucket" "my_bucket" {
   name     = "fdsblsdjnds"
   location = "US"
 }
-
-

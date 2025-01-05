487c7ca7db733c6e5b2d96bb9d3aa9789d46d247
Kuldeep701 <145199980+Kuldeep701@users.noreply.github.com>
2025-01-05 16:03:49 +0530

    updated backend

--- main.tf
+++ main.tf
@@ -3,14 +3,17 @@ provider "google" {
   region  = "us-central1"
 }
 
+terraform {
+  backend "gcs" {
+    bucket = "statefile3957"
+    prefix = "terraform_state"
+    region  = "us-central1"
+  }
+}
+
 resource "google_storage_bucket" "my_bucket" {
   name     = "fdsblsdjnds"
   location = "US"
 }
 
-terraform {
-  backend "gcs" {
-    bucket = "fdsblsdjnds"
-    prefix = "terraform/state"
-  }
-}
+
